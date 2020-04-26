48f2a048890eb348a825d53ac43ab919e5389712
shakyStone <64257091+shakyStone@users.noreply.github.com>
2020-04-26 10:25:06 +0900

    練習解答

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'user strict';
 function add(numbers) {
     let result = 0;
     for (let num of numbers) {
@@ -6,6 +6,18 @@ function add(numbers) {
     }
     return result;
 }
+
+let multi = n => {
+    let m = 1;
+    let i = 0;
+    while(i < n.length) {
+        m *= n[i];
+        i++;
+    }
+    return m;
+}
+
 module.exports = {
-    add: add
+    add: add,
+    multi: multi
 };
